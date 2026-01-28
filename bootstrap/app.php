b602a3511d0fc29417bcce3ca9393d12d0d32140
akashRbn <akashrbn610@gmail.com>
2026-01-28 13:35:21 +0600

    first commit

---
+++ bootstrap/app.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Agent;
+use App\Http\Middleware\Student;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Authenticate;
+use Illuminate\Support\Facades\Auth;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'admin'        => Admin::class,
+            'agent'       => Agent::class,
+            'student'      => Student::class,
+            'authenticate' => Authenticate::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
+
+
+    
